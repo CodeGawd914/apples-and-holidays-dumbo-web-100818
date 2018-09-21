230fd8c5ef5d5671bbaf2a90ee8dbdbd1c031b32
Travis Desir <travisgdesir31@gmail.com>
2018-09-21 18:14:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, item|
       if item.include? ("BBQ")
-        seasons.push(holiday)
+        seasons.push{(holiday)
       end
       end
     end
