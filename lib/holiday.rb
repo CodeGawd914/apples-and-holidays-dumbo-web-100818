2d98dd048f71da2a0ad0e438eb18ee1133985e88
Travis Desir <travisgdesir31@gmail.com>
2018-09-21 17:55:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,6 +46,7 @@ supply.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
