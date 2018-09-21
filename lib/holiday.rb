357d9f0d9faebfa3c9a5e1ce57548ab4d38bea95
Travis Desir <travisgdesir31@gmail.com>
2018-09-21 18:05:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").each {|word| word.capitalize}.join(" ")<< ":"
+      puts "  " + holiday + " " + supplies
   end
 end
 end
