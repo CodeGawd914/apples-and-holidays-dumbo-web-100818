402f3c944d2201ccab496626ddbe6b331bf918dd
Travis Desir <travisgdesir31@gmail.com>
2018-09-21 18:10:44 +0000

    automatically backed up by learn

---
+++ lib/holiday.rb
@@ -0,0 +1,70 @@
+require 'pry'
+
+def second_supply_for_fourth_of_july(holiday_hash)
+  # given that holiday_hash looks like this:
+  # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
+  # return the second element in the 4th of July array
+ return holiday_hash[:summer][:fourth_of_july][1]
+end
+
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter].each do |x,y|
+    y << supply
+  end
+end
+
+
+def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring].each do |x,y|
+    y << supply
+  end
+end
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+end
+
+def all_winter_holiday_supplies(holiday_hash)
+supply = holiday_hash[:winter].values
+supply.flatten
+end
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").each {|word| word.capitalize!}.join(" ")<< ":"
+      puts "  " + holiday + " " + supplies
+  end
+end
+end
+
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, item|
+      if item 
+end
+
+
+
+
+
+
+
